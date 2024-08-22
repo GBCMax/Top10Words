b07610fd5bb8f90a0b3549e4734213f5ecd2b161
max_on <maks_on_max@mail.ru>
2024-08-22 14:24:45 +0700

    Добавьте файлы проекта.

---
+++ Top10Words/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+  private static Dictionary<string, int> _words = [];
+  private static void Main(string[] args)
+  {
+    using var sr = new StreamReader("Data\\Text2.txt");
+
+    string text = sr.ReadToEnd();
+
+    var noPunctuationText = new string(text.Where(x => !char.IsPunctuation(x)).ToArray()).Trim();
+
+    noPunctuationText = noPunctuationText.Replace("  ", " ");
+
+    foreach (var word in noPunctuationText.Split(' '))
+    {
+      if (_words.ContainsKey(word.ToLower()))
+      {
+        _words[word.ToLower()]++;
+      }
+      else
+      {
+        _words.Add(word.ToLower(), 1);
+      }
+    }
+
+    var topWords = _words.OrderBy(w => w.Value).Reverse().Take(10);
+
+    Console.WriteLine($"Топ 10 слов:");
+
+    foreach (var word in topWords)
+    {
+      Console.WriteLine($"{word.Key} - {word.Value}");
+    }
+  }
+}
